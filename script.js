22d350a3617d3201d93bfa843196a914f6ddf612
Taylor Jennings <taylor.jennings128@gmail.com>
2023-06-12 21:28:01 -0600

    updated rps for ui

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-//Player selection
+let rockChoice = document.querySelector("#rock");
+let paperChoice = document.querySelector("#paper");
+let scissorsChoice = document.querySelector("#scissors");
+
 let playerSelection = "";
-const getPlayerSelection = () => {
-  let playerSelection = prompt(
-    'Please enter "Rock, Paper, or Scissors'
-  ).toLowerCase();
-  return playerSelection;
-};
+
+console.log("Use the button to choose your selection");
+rockChoice.addEventListener("click", function () {
+  playerSelection = "rock";
+  console.log(playerSelection);
+  playRound(playerSelection);
+});
+paperChoice.addEventListener("click", function () {
+  playerSelection = "paper";
+  console.log(playerSelection);
+  playRound(playerSelection);
+});
+scissorsChoice.addEventListener("click", function () {
+  playerSelection = "scissors";
+  console.log(playerSelection);
+  playRound(playerSelection);
+});
 
 //Computer selection
 let computerChoice = "";
@@ -22,67 +36,71 @@ const getComputerChoice = () => {
   return computerSelection;
 };
 
-//save function returns
-// computerSelection = getComputerChoice();
-// playerSelection = getPlayerSelection();
-// console.log(computerSelection, playerSelection);
-
-//win logic---switch statement??
-//counters
 let comp = 0;
 let player = 0;
 
 const playRound = (playerSelection, computerSelection) => {
   computerSelection = getComputerChoice();
-  playerSelection = getPlayerSelection();
   console.log(computerSelection, playerSelection);
+  let result = "";
+
   if (playerSelection === "rock" && computerSelection === "rock") {
     console.log("It's a tie!");
+    result = "It's  a tie";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     console.log("paper beats rock");
+    result = "paper beats rock";
     comp++;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     console.log("rock beats scissors");
+    result = "rock beats scissors";
     player++;
     //paper
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     console.log("paper beats rock");
+    result = "paper beats rock";
     player++;
   } else if (playerSelection === "paper" && computerSelection === "paper") {
     console.log("It's a tie");
+    result = "It's a tie";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     console.log("scissors beat papper");
+    result = "scissors beat papper";
     comp++;
     //scissors
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     console.log("rock beats scissors");
+    result = "rock beats scissors";
     comp++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log("scissors beats paper");
+    result = "scissors beats paper";
     player++;
   } else if (
     playerSelection === "scissors" &&
     computerSelection === "scissors"
   ) {
     console.log("It's a tie!");
+    result = "It's a tie!";
+  }
+
+  //who won round display
+  let display = document.querySelector(".display");
+  display.innerText = `You chose ${playerSelection}, computer chose ${computerSelection}`;
+  let displayP = document.createElement("p");
+  displayP.innerText = result;
+  display.appendChild(displayP);
+
+  //display score
+  let score = document.querySelector(".score");
+  let scoreBoard = `Player score: ${player}, Computer Score: ${comp}`;
+  score.innerText = scoreBoard;
+
+  if (player === 5) {
+    display.innerText = `Congrats! Player Won!`;
+  } else if (comp === 5) {
+    display.innerText = `Uh oh, Computer took this round`;
   }
 };
 
-//while loop play again
-//Insert counter up to 3, then ask if you want to play again and reset counter
-// let gameOn = true;
-// while (gameOn === true) {
-//   playRound();
-//   console.log(`Player wins ${player} times, Computer wins ${comp} times`);
-//   let yahsNo = prompt("Play again? Y/N").toLowerCase();
-//   if (yahsNo === "n") {
-//     gameOn = false;
-//   }
-// }
-
-for (let i = 0; i < 5; i++) {
-  playRound();
-  if (i === 4) {
-    console.log(`Player wins ${player} times, Computer wins ${comp} times`);
-  }
-}
+//reset logic?//play again
